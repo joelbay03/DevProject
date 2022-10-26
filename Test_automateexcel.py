7773268d05fb4be458db4d05e9463b386387c6a9
joelbay03 <90229527+joelbay03@users.noreply.github.com>
2022-10-26 09:31:44 -0700

    Add files via upload

---
+++ Test_automateexcel.py
@@ -0,0 +1,10 @@
+from openpyxl import workbook, load_workbook
+
+wb=load_workbook('daily_overview(FKA 2hr).xlsx')
+
+wb.create_sheet("Test")
+print(wb.sheetnames)
+
+wb.save('daily_overview(FKA 2hr).xlsx')
+#ws = wb.active
+#print(ws['C2'].value)
